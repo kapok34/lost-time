1948a8dbb38005a365cf0c4599abf7c71217a25a
kapok34 <nekihas@proton.me>
2026-05-04 20:24:27 +0200

    Rename Login.tsx to Connexion.tsx and update component name and French text.

---
+++ src/pages/Connexion.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Connexion = () => {
+    return (
+        <div>
+            <h1>Connexion</h1>
+            <p>Veuillez entrer vos informations de connexion.</p>
+        </div>
+    );
+};
+
+export default Connexion;
